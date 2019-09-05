fc0e6d4707764acedaa384e970e32b606d774ed9
ipwhois-io <47532184+ipwhois-io@users.noreply.github.com>
2019-09-05 20:55:52 +0300

    Update example.php

--- example.php
+++ example.php
@@ -1,4 +1,5 @@
 <?php
+    require_once('ip-geolocation-api.php');  
     $ip = "8.8.4.4"; // CLIENT IPADDRESS
     $location = get_ipwhois($ip);
     $decodedLocation = json_decode($location, true);
@@ -6,18 +7,4 @@
     echo "<pre>";
     print_r($decodedLocation);
     echo "</pre>";
-
-    function get_ipwhois($ip, $format = "json", $lang = "en") {
-        $url = "http://free.ipwhois.io/".$format."/".$ip."?lang=".$lang;
-        $cURL = curl_init();
-
-        curl_setopt($cURL, CURLOPT_URL, $url);
-        curl_setopt($cURL, CURLOPT_HTTPGET, true);
-        curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/json',
-            'Accept: application/json'
-        ));
-        return curl_exec($cURL);
-    }
 ?>
